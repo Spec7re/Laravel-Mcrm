3f84ca5ac5de6d47615f37572744b94a940711cd
Spec7re <dev.andonov@gmail.com>
2021-12-13 16:45:40 +0200

    Replace missing icons.

--- resources/views/auth/employee-login.blade.php
+++ resources/views/auth/employee-login.blade.php
@@ -29,9 +29,9 @@
 
         <form class="form-signin" method="POST" action="{{ route('employee.login.submit') }}">
             {{ csrf_field() }}
-
-
-            <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+            <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 0 16 16">
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
+            </svg>
             <h1 class="h3 mb-3 font-weight-normal">EMPLOYEE Sing in </h1>
 
             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
