31f131f08f44e5846f7f50fc66cd9bc23f0223a0
Spec7re <dev.andonov@gmail.com>
2021-12-14 18:03:22 +0200

    Update button links, update git ignore.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,14 +84,13 @@
                 </div>
 
                 <div class="links">
-                    <a href="http://mycrm.local/company/login">Company</a>
+                    <a href="{{ route('company.login') }}">Company</a>
+                    <a href="{{ route('employee.login') }}">Employee</a>
                     {{--<a href="https://laravel.com/docs">Documentation</a>--}}
-                    <a href="https://laracasts.com">Laracasts</a>
                     {{--<a href="https://laravel-news.com">News</a>--}}
                     {{--<a href="https://forge.laravel.com">Forge</a>--}}
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-
-                    <a href="http://mycrm.local/employee/login">Employee</a>
+                    <a href="https://laracasts.com">Laracasts</a>
                 </div>
             </div>
         </div>
